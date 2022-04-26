d2bc3ed8565080b7d0302d62b5d88cdec73da4f6
DwiPanggaSinurat <91214824+DwiPanggaSinurat@users.noreply.github.com>
2022-04-26 21:44:09 +0700

    Update DaftarBarangPegawai.js

--- Page1/DaftarBarangPegawai.js
+++ Page1/DaftarBarangPegawai.js
@@ -10,8 +10,12 @@ import {
     deleteDoc,
     doc, onSnapshot,
 } from "firebase/firestore";
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function DaftarBarangPegawai({ navigation }) {
+    
+    const [modalVisible1, setModalVisible1] = useState(false);
+    const [modalVisible2, setModalVisible2] = useState(false);
 
     const [users, setUsers] = useState([]);
     const x = "Produk";
@@ -25,7 +29,184 @@ export default function DaftarBarangPegawai({ navigation }) {
         []
     );
     return (
+        
         <View style={styles.container}>
+            <Modal
+                animationType="none"
+                transparent={true}
+                visible={modalVisible2}
+            >
+                <View style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    marginTop: 22
+                }}>
+
+                    <View style={{
+                        backgroundColor: "white",
+                        flexDirection: "colum",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        paddingTop: "7%",
+                        paddingBottom: "3%",
+                        marginLeft:"8%",
+                        marginRight:"8%", 
+                        borderRadius: 10,
+                        shadowColor: "#000",
+                        shadowOffset: {
+                        width: 0,
+                        height: 2},
+                        shadowOpacity: 0.25,
+                        shadowRadius: 4
+                    }}>
+
+                    <View style={{ marginBottom: "5%", paddingRight: "5%" }} >
+                        <Text style={{
+                            fontWeight: 650,
+                            fontSize: "90%",
+                            color: "black"
+                        }}> Apakah Anda yakin ingin keluar ?
+                        </Text>
+                        </View>
+
+                        <View style={{
+                            flexDirection: "row",
+                            width: "65%",
+                            marginTop: "5%",
+                            marginBottom: "8%"
+                        }}>
+                            <TouchableOpacity style={{
+                                flex: 1,
+                                backgroundColor: "white",
+                                borderRadius: 3,
+                                borderWidth: 1,
+                                borderColor: "#F24E1E",
+                                padding: 5,
+                                justifyContent: "center",
+                                alignItems: "center",
+                                marginLeft: "10%",
+                                width: "40%"
+                            }}
+                            onPress={() => navigation.navigate("Login")}>
+                                <Text style={{
+                                    fontWeight: 700,
+                                    fontSize: "90%",
+                                    color: "#F24E1E",
+                                }}> Ya </Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity style={{
+                                flex: 1,
+                                backgroundColor: "#F24E1E",
+                                borderRadius: 3,
+                                padding: 5,
+                                justifyContent: "center",
+                                alignItems: "center",
+                                marginLeft: "10%",
+                                width: "40%"
+                            }}
+                                onPress={() => setModalVisible2(!modalVisible2)}>
+                                <Text style={{
+                                    fontWeight: 700,
+                                    fontSize: "90%",
+                                    color: "white",
+                                }}> Tidak </Text>
+                            </TouchableOpacity>
+                        </View>
+
+                    </View>
+
+                    <StatusBar style="auto" />
+                </View>
+            </Modal>
+                    
+            <Modal
+                animationType="none"
+                transparent={true}
+                visible={modalVisible1}
+            >
+                <View style={{
+                flex: 1,
+                justifyContent: "center",
+                marginTop: 22
+                }}>
+
+                <View style={{
+                    backgroundColor: "white",
+                    flexDirection: "colum",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    paddingTop: "7%",
+                    paddingBottom: "3%",
+                    marginLeft:"8%",
+                    marginRight:"8%", 
+                    borderRadius: 10,
+                    shadowColor: "#000",
+                    shadowOffset: {
+                    width: 0,
+                    height: 2},
+                    shadowOpacity: 0.25,
+                    shadowRadius: 4
+                }}>
+
+                    <View style={{ marginBottom: "5%", paddingRight: "28%" }} >
+                    <Text style={{
+                        fontWeight: 650,
+                        fontSize: "90%",
+                        color: "black",
+                    }}> Apakah Anda yakin ?
+                    </Text>
+                    </View>
+
+                    <View style={{
+                        flexDirection: "row",
+                        width: "65%",
+                        marginTop: "5%",
+                        marginBottom: "8%"
+                    }}>
+                    <TouchableOpacity style={{
+                        flex: 1,
+                        backgroundColor: "#F24E1E",
+                        borderRadius: 3,
+                        padding: 5,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        width: "40%"
+                    }}>
+                        <Text style={{
+                        fontWeight: 700,
+                        fontSize: "90%",
+                        color: "white",
+                        }}> Ya </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={{
+                        flex: 1,
+                        backgroundColor: "white",
+                        borderRadius: 3,
+                        borderWidth: 1,
+                        borderColor: "#F24E1E",
+                        padding: 5,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        marginLeft: "10%",
+                        width: "40%"
+                    }}
+                        onPress={() => setModalVisible1(!modalVisible1)}>
+                        <Text style={{
+                        fontWeight: 700,
+                        fontSize: "90%",
+                        color: "#F24E1E",
+                        }}> Tidak </Text>
+                    </TouchableOpacity>
+                    </View>
+
+                </View>
+
+                <StatusBar style="auto" />
+                </View>
+            </Modal>
+
             <View style={{
                 flexDirection: "row",
                 marginTop: "8%",
@@ -33,42 +214,65 @@ export default function DaftarBarangPegawai({ navigation }) {
                 borderBottomColor: 'lightgray',
                 borderBottomWidth: 1
             }}>
+                
+                <TouchableOpacity style={{
+                    backgroundColor: "#F24E1E",
+                    borderRadius: 3,
+                    marginLeft: '1%',
+                    padding: 5,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}
+                    onPress={() => setModalVisible2(true)}> 
+                    <Image
+                        source={require('../assets/Logout.png')}
+                        style={{
+                            height: 17,
+                            width: 17,
+                            marginBottom: "4%"
+                        }}>
+                    </Image>
+                </TouchableOpacity>
+                
                 <TouchableOpacity style={{
                     backgroundColor: "#F24E1E",
                     borderRadius: 3,
                     padding: 5,
-                    marginLeft: '2%',
-                    marginRight: 20,
+                    marginLeft: '1.5%',
                     justifyContent: "center",
                     alignItems: "center",
                     width: "40%"
                 }} onPress={() => navigation.navigate("MenuProdukPegawai")}>
                     <Text style={{
                         fontWeight: 700,
-                        fontSize: 12,
+                        fontSize: "80%",
                         color: "white",
-                    }}> Menu Produk </Text>
+                    }}> Menu Penjualan </Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity style={{
-                    backgroundColor: "#F24E1E",
+                    backgroundColor: "#white",
                     borderRadius: 3,
+                    borderWidth:2,
+                    borderColor:"#F24E1E",
                     padding: 5,
-                    marginRight: 20,
+                    marginLeft: '1.5%',
                     justifyContent: "center",
                     alignItems: "center",
                     width: "40%"
                 }} onPress={() => navigation.navigate("DaftarBarangPegawai")}>
                     <Text style={{
                         fontWeight: 700,
-                        fontSize: 12,
-                        color: "white",
-                    }}> Daftar Produk </Text>
+                        fontSize: "80%",
+                        color: "#F24E1E",
+                    }}> Daftar Barang </Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity style={{
                     backgroundColor: "#F24E1E",
                     borderRadius: 3,
+                    marginLeft: '1.5%',
+                    marginRight:"1%",
                     padding: 5,
                     justifyContent: "center",
                     alignItems: "center"
@@ -86,7 +290,7 @@ export default function DaftarBarangPegawai({ navigation }) {
 
 
             </View>
-
+            
             <DataTable>
                 <DataTable.Header>
                     <DataTable.Title>ID</DataTable.Title>
@@ -95,6 +299,8 @@ export default function DaftarBarangPegawai({ navigation }) {
                     <DataTable.Title >Harga</DataTable.Title>
                 </DataTable.Header>
             </DataTable>
+
+            <ScrollView>
             {users.map((user) => {
                 return (
                     <DataTable>
@@ -107,6 +313,8 @@ export default function DaftarBarangPegawai({ navigation }) {
                     </DataTable>
                 );
             })}
+            </ScrollView>
+
             <StatusBar style="auto" />
         </View >
     );
