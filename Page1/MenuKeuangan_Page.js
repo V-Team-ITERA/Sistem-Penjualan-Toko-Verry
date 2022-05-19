d42337923213f7d313b7e3bf70745957c635a923
MarioV27 <84635915+MarioV27@users.noreply.github.com>
2022-05-19 21:08:44 +0700

    Update MenuKeuangan_Page.js

--- Page1/MenuKeuangan_Page.js
+++ Page1/MenuKeuangan_Page.js
@@ -6,10 +6,7 @@ import { DataTable } from 'react-native-paper';
 import { db } from "../firebase.js";
 import {
   collection,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-  doc, onSnapshot,
+  onSnapshot,
 } from "firebase/firestore";
 import { ScrollView } from "react-native-gesture-handler";
 
@@ -21,6 +18,7 @@ export default function MenuKeuangan({ navigation }) {
   const [newbulan, setNewbulan] = useState(month);
   const [newtahun, setNewtahun] = useState(year);
   let indexbulan = 0;
+  let total = 0;
   const bulan = [
     { key: indexbulan++, label: 'January' },
     { key: indexbulan++, label: 'February' },
@@ -46,6 +44,11 @@ export default function MenuKeuangan({ navigation }) {
     { key: indextahun++, label: year + 3 },
   ]
 
+  const cekkeuangan = async () => {
+    setModalVisible(false);
+    navigation.navigate("LihatKeuangan", { time: newbulan + newtahun });
+  }
+
   const usersCollectionRef2 = collection(db, newbulan + newtahun);
   const [users, setUsers] = useState([]);
   useEffect(
@@ -68,7 +71,7 @@ export default function MenuKeuangan({ navigation }) {
           flex: 1,
           justifyContent: "center",
           marginTop: 22,
-         
+
         }}>
 
           <View style={{
@@ -78,13 +81,14 @@ export default function MenuKeuangan({ navigation }) {
             alignItems: "center",
             paddingTop: "7%",
             paddingBottom: "3%",
-            marginLeft:"8%",
-            marginRight:"8%", 
+            marginLeft: "8%",
+            marginRight: "8%",
             borderRadius: 10,
             shadowColor: "#000",
             shadowOffset: {
-            width: 0,
-            height: 2},
+              width: 0,
+              height: 2
+            },
             shadowOpacity: 0.25,
             shadowRadius: 4
           }}>
@@ -112,7 +116,7 @@ export default function MenuKeuangan({ navigation }) {
                 justifyContent: "center",
                 alignItems: "center",
                 width: "40%"
-              }}>
+              }} onPress={() => cekkeuangan()}>
                 <Text style={{
                   fontWeight: 700,
                   fontSize: "90%",
@@ -146,8 +150,6 @@ export default function MenuKeuangan({ navigation }) {
           <StatusBar style="auto" />
         </View>
       </Modal>
-
-
 
       <View style={{
         flexDirection: "row",
@@ -183,19 +185,20 @@ export default function MenuKeuangan({ navigation }) {
       </View>
 
       <View style={{
-              flexDirection: "row",
-              width: "80%",
-              marginLeft:"8%",
-              marginTop: "1%",
-              marginBottom: "1%"
-            }}>
+        flexDirection: "row",
+        width: "80%",
+        marginLeft: "8%",
+        marginTop: "1%",
+        marginBottom: "1%"
+      }}>
         <ModalSelector
-          style={{  flex: 1,
-                    borderRadius: 3,
-                    backgroundColor: "#F24E1E",
-                    padding: 5,
-                    margin: "5%"
-                  }}
+          style={{
+            flex: 1,
+            borderRadius: 3,
+            backgroundColor: "#F24E1E",
+            padding: 5,
+            margin: "5%"
+          }}
           data={bulan}
           onChange={(option) => {
             setNewbulan(option.label);
@@ -205,10 +208,12 @@ export default function MenuKeuangan({ navigation }) {
           }}
         >
           <TextInput
-            style={{  marginLeft:"10%",
-                      fontWeight: 700,
-                      fontSize: "105%",
-                      color: "white"}}
+            style={{
+              marginLeft: "10%",
+              fontWeight: 700,
+              fontSize: "105%",
+              color: "white"
+            }}
             editable={false}
             value={newbulan}
           />
@@ -217,11 +222,12 @@ export default function MenuKeuangan({ navigation }) {
 
 
         <ModalSelector
-          style={{  flex: 0.7,
-                    borderRadius: 3,
-                    backgroundColor: "#F24E1E",
-                    padding: 5,
-                    margin: "5%"
+          style={{
+            flex: 0.7,
+            borderRadius: 3,
+            backgroundColor: "#F24E1E",
+            padding: 5,
+            margin: "5%"
           }}
           data={tahun}
           onChange={(option) => {
@@ -232,19 +238,22 @@ export default function MenuKeuangan({ navigation }) {
           }}
         >
           <TextInput
-            style={{  marginLeft:"30%",
-                      fontWeight: 700,
-                      fontSize: "105%",
-                      color: "white"}}
+            style={{
+              marginLeft: "30%",
+              fontWeight: 700,
+              fontSize: "105%",
+              color: "white"
+            }}
             editable={false}
             value={newtahun}
           />
         </ModalSelector>
-        </View>
+      </View>
 
       <View style={{
-              borderBottomColor: 'lightgray',
-              borderBottomWidth: 1}}>
+        borderBottomColor: 'lightgray',
+        borderBottomWidth: 1
+      }}>
 
       </View>
       <DataTable>
@@ -258,58 +267,81 @@ export default function MenuKeuangan({ navigation }) {
 
       <ScrollView>
         {users.map((user) => {
-          return (
+          if (user.Total != 0) {
+            total = total + user.Total;
+            return (
               <DataTable>
                 <DataTable.Row>
                   <DataTable.Cell>{user.Tanggal}</DataTable.Cell>
                   <DataTable.Cell>{user.IdNota}</DataTable.Cell>
                   <DataTable.Cell>{user.Total}</DataTable.Cell>
                   <DataTable.Cell>
-                    
-                    <TouchableOpacity style={{borderWidth:1,
-                                              backgroundColor: "white",
-                                              borderRadius: 3,
-                                              padding: 5,
-                                              justifyContent: "center",
-                                              alignItems: "center",
-                                              width: "100%"
-                                            }}
-                    onPress={() => navigation.navigate("LihatNota", { time: user.IdNota })}>
-                    <Text style={{
-                      fontWeight: 600,
-                      fontSize: "85%",
-                      color: "black",
-                    }}> Detail </Text>
-                  </TouchableOpacity>
+
+                    <TouchableOpacity style={{
+                      borderWidth: 1,
+                      backgroundColor: "white",
+                      borderRadius: 3,
+                      padding: 5,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      width: "100%"
+                    }}
+                      onPress={() => navigation.navigate("LihatNota", { time: user.IdNota })}>
+                      <Text style={{
+                        fontWeight: 600,
+                        fontSize: "85%",
+                        color: "black",
+                      }}> Detail </Text>
+                    </TouchableOpacity>
                   </DataTable.Cell>
-                </DataTable.Row>   
+                </DataTable.Row>
               </DataTable>
-          );
+            );
+          }
         })}
       </ScrollView>
 
 
-      <View style={{  borderTopColor: 'lightgray',
-                      borderTopWidth: 1}}>
-      <TouchableOpacity style={{
-        backgroundColor: "#F24E1E",
-        borderRadius: 3,
-        margin:"7%",
-        marginLeft:"27%",
-        padding: 5,
-        justifyContent: "center",
-        alignItems: "center",
-        width: "40%"
-      }}
-        onPress={() => setModalVisible(true)}>
-        <Text style={{
-          fontWeight: 700,
-          fontSize: "95%",
-          color: "white",
-        }}> Cetak </Text>
-      </TouchableOpacity>
+      <View style={{
+        borderTopColor: 'lightgray',
+        borderTopWidth: 1
+      }}>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={{
+            flex: 1,
+            fontWeight: 700,
+            fontSize: "95%",
+            color: "black",
+            margin: "3%",
+          }}>Total</Text>
+
+          <Text style={{
+            flex: 1,
+            fontWeight: 500,
+            fontSize: "95%",
+            color: "black",
+            margin: "3%",
+          }}> Rp.  {total} </Text>
+        </View>
+        <TouchableOpacity style={{
+          backgroundColor: "#F24E1E",
+          borderRadius: 3,
+          margin: "7%",
+          marginLeft: "27%",
+          padding: 5,
+          justifyContent: "center",
+          alignItems: "center",
+          width: "40%"
+        }}
+          onPress={() => setModalVisible(true)}>
+          <Text style={{
+            fontWeight: 700,
+            fontSize: "95%",
+            color: "white",
+          }}> Cetak </Text>
+        </TouchableOpacity>
       </View>
-      
+
 
       <StatusBar style="auto" />
     </View>
